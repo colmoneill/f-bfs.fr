fc31d306d0630bc7849b8605de3b7a8a1240e936
colm <mail@colm.be>
2015-03-09 14:38:22 +0100

    cleaning up the suggested setup :: almost 20 different partial stylesheets is a reciepe to get lost in someone elses structure. clean clean clean

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<p class="copyright">&copy; <?php echo date( "Y" ); echo " "; bloginfo( 'name' ); ?></p>
+			<p class="copyright"><?php echo date( "Y" ); echo "&nbsp; Fred & breakfast"; ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,5 @@
+<!--
+
 <?php
 /**
  * The sidebar containing the main widget area.
@@ -13,3 +15,5 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 <div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
+
+-->
