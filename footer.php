364681b85b750802102d5a0144d269f198e6b482
colm <mail@colm.be>
2015-04-14 19:37:11 +0200

    making the footer while waiting for the site to deploy

--- footer.php
+++ footer.php
@@ -12,6 +12,37 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+			 <form action="/" method="get">
+				<fieldset>
+					<label for="search"></label>
+					<input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
+					<input type="image" alt="&rarr; Rechercher" src="<?php bloginfo( 'template_url' ); ?>/images/search.png" />
+				</fieldset>
+			</form>
+			<div class="columns">
+				<div class="oneoffour">
+					Visites dans Paris sur l'architecture
+				</div>
+				<div class="oneoffour">
+					Visites dans Paris sur l'architecture
+				</div>
+				<div class="oneoffour">
+					<ul>
+						<lh>Réseaux sociaux</lh>
+						<li>Facebook</li>
+						<li>Twitter</li>
+						<li>Pinterest</li>
+						<li>Instagram</li>
+					</ul>
+				</div>
+				<div class="oneoffour">
+					<ul>
+					<lh>Mentions légales</lh>
+					<li>Site web: <a href="http://colm.be">Colm O'Neill</a></li>
+					<li>Graphisme: <a href="http://alicedc.fr/">Alice Dillon Corneck</a></li>
+					</ul>
+				</div>
+			</div>
 			<p class="copyright"><?php echo date( "Y" ); echo "&nbsp; Fred & breakfast"; ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
