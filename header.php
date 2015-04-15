9464659a3586400803e82d7913f3b967efac5fd5
colm <mail@colm.be>
2015-04-15 19:37:25 +0200

    Today's work was mainly on the server, syncing plugins, installing the calendar, translating menus, etc, but I'm still committing this tiny change to output the language switcher in the header and not the footer. Options for pll_the_languages are great. Pushing has absolutely nothing to do with keeping up my GH streak. Nothing at all. #sarcasm in commit messages

--- header.php
+++ header.php
@@ -24,11 +24,13 @@
 	<![endif]-->
 
 	<header id="masthead" class="site-header" role="banner">
+		<div class="languages">
+			<ul><?php pll_the_languages(array('display_names_as'=> 'slug')); ?></ul>
+		</div>
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
-
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Menu', '_mbbasetheme' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
