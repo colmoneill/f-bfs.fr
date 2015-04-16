e25f820314a26f0d885d1738059e39a31b85974a
colm <mail@colm.be>
2015-04-16 11:08:51 +0200

    menu row

--- header.php
+++ header.php
@@ -36,6 +36,7 @@
 			<button class="menu-toggle"><?php _e( 'Menu', '_mbbasetheme' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
+	<hr>
 	</header><!-- #masthead -->
 
 
