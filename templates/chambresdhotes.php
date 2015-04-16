53de2e7b4232628eb7654e49238ea54f4fcb2ef9
colm <mail@colm.be>
2015-04-16 12:53:31 +0200

    a template file specific for each presenation page, and the calendar styles getting ready.

--- templates/chambresdhotes.php
+++ templates/chambresdhotes.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: content
+ * Template Name: Chambres d'hôtes
  *
  * Displays content for content page layouts
  *
@@ -31,6 +31,14 @@ get_header(); ?>
 				<div id="sidematter" class="widget-area" role="complementary">
 				<?php dynamic_sidebar( 'sidebar-1' ); ?>
 				</div>
+				</div>
+				<div class="calendars">
+					<div class="left-calendar">
+						<?php echo do_shortcode('[dopbsp id=1]'); ?>
+					</div>
+					<div class="right-calendar">
+						<?php echo do_shortcode('[dopbsp id=2]'); ?>
+					</div>
 				</div>
 			<?php endwhile; // end of the loop. ?>
 
