f72e885bfa4291e51a157e47452391a56184494e
colm <mail@colm.be>
2015-04-16 22:58:59 +0200

    organising the templates, reoganising the footer of each to display different calendars. It's hard coded for the mo, might have to think of a custom field to output them, depending on how complex the classes are going to be

---
+++ templates/coursdecuisine.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: Cours de cuisine
+ *
+ * Displays content for content page layouts
+ *
+ * @package _mbbasetheme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="hero-container">
+					<img class="hero" src="<?php the_field('full_width_image'); ?>" />
+				</div>
+
+				<div class="title">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</div>
+
+				<div class="article-sidematter">
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+					return;
+				}
+				?>
+				<div id="sidematter" class="widget-area" role="complementary">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+				</div>
+				</div>
+				<div class="calendars-container">
+					<div class="calendars no-detail-display">
+						<div class="title">
+							<h1 class="entry-title">Reservations</h1>
+						</div>
+						<div class="center-calendar">
+							<h2>Class1</h2>
+							<p>1&#151;2 ppl</p>
+							<?php echo do_shortcode('[dopbsp id="3"]'); ?>
+							<a class="reservation-button" href="#">make a reservation</a>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
