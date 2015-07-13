f7a06bfb95f334fb22d18740fa9c0d0742901fcc
colm <mail@colm.be>
2015-07-13 16:48:44 +0200

    general blog post listing page taking shape

--- templates/blog.php
+++ templates/blog.php
@@ -11,6 +11,11 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<div class="post-list-container">
+			<div class="post_categories">
+				<ul>
+					<?php wp_list_categories('title_li='); ?>
+				</ul>
+			</div>
 		<?php $myposts = get_posts('');
 		foreach($myposts as $post) :
 		setup_postdata($post); ?>
@@ -19,13 +24,15 @@ get_header(); ?>
 					<h1 class="post-title">
 						<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 					</h1>
+					<p><?php the_tags(); ?></p>
+					<!--
 					<p class="post-meta">Posted by <?php the_author(); ?></p>
 					<small><?php _mbbasetheme_posted_on(); ?></small>
-
+				-->
 				</div>
-				<hr>
 		<?php endforeach; wp_reset_postdata(); ?>
 			</div>
+			<?php the_tags(); ?>
 		</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
