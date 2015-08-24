81c28fb2d0ccf2c4688959edea2252ef3e6b3cda
colm <mail@colm.be>
2015-08-24 18:20:24 +0200

    the referenced styles for the sliders and the structure to welcome up to 6 sliders

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -4,8 +4,8 @@ jQuery(document).ready(function ($) {
 
 				$PauseOnHover: 1,                               //[Optional] Whether to pause when mouse over if a slideshow is auto playing, default value is false
 
-				$ArrowKeyNavigation: true,   			            //Allows arrow key to navigate or not
-				$SlideWidth: 400,                                   //[Optional] Width of every slide in pixels, the default is width of 'slides' container
+				//$ArrowKeyNavigation: true,   			            //Allows arrow key to navigate or not
+				$SlideWidth: 625,                                   //[Optional] Width of every slide in pixels, the default is width of 'slides' container
 				//$SlideHeight: 500,                                  //[Optional] Height of every slide in pixels, the default is width of 'slides' container
 				$SlideSpacing: 20, 					                //Space between each slide in pixels
 				$DisplayPieces: 2,                                  //Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
