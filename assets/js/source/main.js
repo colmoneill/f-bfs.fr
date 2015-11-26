7b5f77751e6ff75308cc050a23c6560ef85e3f5c
colm <mail@colm.be>
2015-11-26 17:57:14 +0100

    I don't like doing this bang important stuff, but I can't understand any better at the mo

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function ($) {
 
 				//$ArrowKeyNavigation: true,   			            //Allows arrow key to navigate or not
 				$SlideWidth: 650,                                   //[Optional] Width of every slide in pixels, the default is width of 'slides' container
-				//$SlideHeight: 500,                                  //[Optional] Height of every slide in pixels, the default is width of 'slides' container
+				$SlideHeight: 500,                                  //[Optional] Height of every slide in pixels, the default is width of 'slides' container
 				$SlideSpacing: 20, 					                //Space between each slide in pixels
 				$DisplayPieces: 2,                                  //Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
 				//$ParkingPosition: 0,                                //The offset position to park slide (this options applys only when slideshow disabled).
